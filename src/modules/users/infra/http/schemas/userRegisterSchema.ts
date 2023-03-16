c83c8fcaad8ac8ada7aa3197b5b8076f637e90f4
GustavoJae <gustavojmy29@gmail.com>
2023-03-16 15:42:51 -0300

    :sparkles: phone_validation

--- src/modules/users/infra/http/schemas/userRegisterSchema.ts
+++ src/modules/users/infra/http/schemas/userRegisterSchema.ts
@@ -1,6 +1,11 @@
 import isCpf from '@shared/utils/CpfValidator';
 import { body } from 'express-validator';
 
+function validPhone(phone: string) {
+  const regex = new RegExp('^\\([0-9]{2}\\)((3[0-9]{3}-[0-9]{4})|(9[0-9]{3}-[0-9]{5}))$');
+  return regex.test(phone);
+}
+
 const userRegisterSchema = [
   body('name').isString().trim().notEmpty(),
   body('email')
@@ -10,12 +15,12 @@ const userRegisterSchema = [
   body('password')
     .isLength({ min: 5 })
     .withMessage('Password must be at least 5 characters long'),
-  body('phone').isString().trim().notEmpty(),
+  body('phone').isString().notEmpty().custom(validPhone),
   body('occupation').isString().trim().notEmpty(),
   body('nationality').isString().trim().notEmpty(),
   body('pep').isBoolean(),
   body('monthly_income').isFloat(),
-  body('birthDate').isISO8601(),
+  body('birthDate').isISO8601().toDate(),
   body('cpf').isString().custom(isCpf)
 ];
 
