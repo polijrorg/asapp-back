6fc88bb564805db266b7d3bfd57d688fe2028b83
GustavoJae <gustavojmy29@gmail.com>
2023-03-18 14:31:54 -0300

    :imp: fix/Refresh-Token-merge-conflict

--- src/modules/users/repositories/IRefreshTokensRepository.ts
+++ src/modules/users/repositories/IRefreshTokensRepository.ts
@@ -5,7 +5,7 @@ interface IRefreshTokensRepository {
   create(data: ICreateRefreshTokenDTO): Promise<UserRefreshToken>;
   findByUserId(userId: string): Promise<UserRefreshToken[]>;
   update(data: UserRefreshToken): Promise<UserRefreshToken>;
-  deleteMany(userId: string): Promise<void | undefined>;
+  deleteMany(userId: string): Promise<void>;
   deleteById(id: string): Promise<void>;
 }
 
