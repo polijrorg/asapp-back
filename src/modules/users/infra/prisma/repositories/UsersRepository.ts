51e3190fa7f46e85be445b2d8944588bd93d182f
GustavoJae <gustavojmy29@gmail.com>
2023-04-04 13:42:15 -0300

    sync-branches-6

--- src/modules/users/infra/prisma/repositories/UsersRepository.ts
+++ src/modules/users/infra/prisma/repositories/UsersRepository.ts
@@ -65,4 +65,35 @@ export default class UsersRepository implements IUsersRepository {
 
     return user;
   }
+
+  public async confirm(userId: string, confirmed: boolean): Promise<User> {
+    const user = await this.ormRepository.update({
+      where: { id: userId },
+      data: { confirmed },
+    });
+    return user
+  }
+  public async changePassword(userId: string, password: string): Promise<User> {
+    const user = await this.ormRepository.update({
+      where: { id: userId },
+      data: { password },
+    });
+    return user;
+  }
+
+  public async destroyToken(userId: string): Promise<User> {
+    const user = await this.ormRepository.update({
+      where: { id: userId },
+      data: { restorePasswordToken: null },
+    });
+    return user;
+  }
+
+  public async createToken(userId: string, token: string): Promise<User> {
+    const user = await this.ormRepository.update({
+      where: {id: userId},
+      data: { restorePasswordToken: token },
+    });
+    return user;
+  }
 }
